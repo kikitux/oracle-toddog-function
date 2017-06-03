23d8a968ce5014f7219fe0f9bad4a36b675009a2
Alvaro Miranda Aguilera <kikitux@gmail.com>
2017-06-03 22:09:41 +0200

    if / if to if / else refactor

--- src/toddog_udp.c
+++ src/toddog_udp.c
@@ -9,9 +9,9 @@ void udp(const unsigned short port, char* message)
   memset((char *)&oAddr, 0,sizeof(oAddr));
   oAddr.sin_family      = AF_INET;
   oAddr.sin_port        = htons(port);
-  oAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  oAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);   // set the address for localhost
   if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
-      return;
+    return;
   }
   sendto(sock, message, strlen(message) + 1, MSG_CONFIRM, (struct sockaddr *)&oAddr, iAddrLen);
   return;
