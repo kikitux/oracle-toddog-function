1e5ce1748c5dc45cc55b095cc336967430881d6b
Alvaro Aguilera <a.aguilera@coolblue.nl>
2017-08-22 10:57:25 +0200

    add drop function sql

---
+++ drop_function_toddog.sql
@@ -0,0 +1,7 @@
+DROP FUNCTION eventtoddog;
+DROP FUNCTION gaugetoddog;
+DROP FUNCTION counttoddog;
+DROP PUBLIC SYNONYM eventtoddog;
+DROP PUBLIC SYNONYM f_metrictoddog;
+DROP PUBLIC SYNONYM gaugetoddog;
+DROP PUBLIC SYNONYM counttoddog;
